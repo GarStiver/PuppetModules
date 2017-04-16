842b04f7dee66e8b87b0b3c6df360419a05935c9
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-16 20:54:37 +0300

    Lot of changes to ssh conf

--- thirdhomework/modules/ssh/manifests/init.pp
+++ thirdhomework/modules/ssh/manifests/init.pp
@@ -2,4 +2,18 @@ class ssh {
 	package {"ssh":
 		ensure => installed,
 	}
+	
+	service {'ssh':
+		ensure => "running",
+		enable => "true",
+		require => Package["ssh"],
+	}
+	
+	file {'/etc/ssh/sshd_config':
+		content => template('ssh/sshd_config.erb'),
+		require => Package['ssh'],
+		notify => Service["ssh"],
+		owner => "root",
+		group => "root",
+	}
 }
