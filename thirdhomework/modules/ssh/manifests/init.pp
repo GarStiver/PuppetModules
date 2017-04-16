41b9bdfd6f5118028b43973205c388e77619f53d
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-16 21:20:05 +0300

    Typo fixes

--- thirdhomework/modules/ssh/manifests/init.pp
+++ thirdhomework/modules/ssh/manifests/init.pp
@@ -6,12 +6,12 @@ class ssh {
 	service {"sshd":
 		ensure => "running",
 		enable => "true",
-		require => Package["ssh"],
+		require => Package["openssh-server"],
 	}
 	
 	file {"/etc/ssh/sshd_config":
-		content => template('sshd/sshd_config.erb'),
-		require => Package["ssh"],
+		content => template('ssh/sshd_config.erb'),
+		require => Package["openssh-server"],
 		notify => Service["sshd"],
 	}
 }
